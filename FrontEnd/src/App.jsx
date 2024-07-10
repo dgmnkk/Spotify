182fcf5bafffc6fc92dcc71779eff0651e73062a
dgmnkk <125593353+dgmnkk@users.noreply.github.com>
2024-07-10 22:10:13 +0300

    ArtistDetails page done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,13 +2,15 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './components/home/Home';
+import ArtistDetails from "./components/ArtistDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
+            <Route index element={<Home />} />
+            <Route path="/artists/:id" element={<ArtistDetails />}/>
         </Route>
       </Routes>
     </BrowserRouter>
